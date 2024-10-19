8a206a45e6a86f4ee5425381e74fc88b737ce56f
ewrika <egorkaomsk_2003@mail.ru>
2024-10-19 10:33:00 +0600

    Initial Commit

---
+++ FirVerse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirVerse
+//
+//  Created by Георгий Борисов on 19.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirVerse/FirVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirVerseApp.swift
+//  FirVerse
+//
+//  Created by Георгий Борисов on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
